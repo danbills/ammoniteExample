cde1eeb9287d412b2d282c7d20d59df7f487f5d3
Dan Billings <dbillings@foundationmedicine.com>
2023-10-01 13:15:34 -0400

    ch2 added

---
+++ ml/ch2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+assert sys.version_info >= (3, 7)
+
+from packaging import version
+import sklearn
+
+assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
+
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+
+def load_housing_data():
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents=True, exist_ok=True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+housing = load_housing_data()
+
+housing.describe()
+
+housing.columns
