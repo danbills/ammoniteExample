f29aad7eb653be61e87a0d2e1d6e5b7ce3cb812a
Dan Billings <dbillings@foundationmedicine.com>
2023-11-12 19:44:59 -0500

    ch2

--- ml/ch2.py
+++ ml/ch2.py
@@ -28,4 +28,65 @@ housing = load_housing_data()
 
 housing.describe()
 
-housing.columns
+housing.info()
+
+housing["ocean_proximity"].value_counts()
+
+import matplotlib.pyplot as plt
+
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
+
+import numpy as np
+
+def shuffle_and_split_data(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = shuffle_and_split_data(housing, 0.2)
+len(train_set)
+
+len(test_set)
+
+np.random.seed(42)
+
+from zlib import crc32
+
+def is_id_in_test_set(identifier, test_ratio):
+    return crc32(np.int64(identifier)) < test_ratio * 2**32
+
+def split_data_with_id_hash(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: is_id_in_test_set(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+housing_with_id = housing.reset_index()
+train_set, test_set = split_data_with_id_hash(housing_with_id, 0.2, "index")
+
+housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+train_set, test_set = split_data_with_id_hash(housing_with_id, 0.2, "id")
+
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+test_set["total_bedrooms"].isnull().sum()
+
+from scipy.stats import binom
+
+sample_size = 1000
+ratio_female = 0.511
+proba_too_small = binom(sample_size, ratio_female).cdf(485-1)
+proba_too_large = 1 - binom(sample_size, ratio_female).cdf(535)
+print(proba_too_small + proba_too_large)
+
+housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
+plt.xlabel("income category")
+plt.ylabel("number of districts")
+plt.show()
+
+
+
+
